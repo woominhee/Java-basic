df5d0a8319786c138cca5e3b0e81bc9d0fed0825
woominhee <maya2001bee@naver.com>
2023-03-18 21:15:23 +0900

    01_06_Constants

---
+++ JavaWorkspace/src/chap_01/_06_Constants.java
@@ -0,0 +1,9 @@
+package chap_01;
+
+public class _06_Constants {
+    public static void main(String[] args) {
+        final String KR_COUNTRY_CODE = "+82";  // 국가 번호  // final을 사용하면 값이 변하지 않는 상수로 바뀜
+        // KR_COUNTRY_CODE = "+8282";
+        System.out.println(KR_COUNTRY_CODE);
+    }
+}
