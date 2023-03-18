2f89824ccafbbf19a618edee08fed7883f267080
woominhee <maya2001bee@naver.com>
2023-03-18 20:03:48 +0900

    01_02_DataTypes

---
+++ JavaWorkspace/src/chap_01/_02_DataTypes.java
@@ -0,0 +1,15 @@
+package chap_01;
+
+public class _02_DataTypes {
+    public static void main(String[] args) {  // 단축키 psvm 또는 main
+        System.out.println("Hello World");  // 단축키 sout  // 문자열
+        System.out.println("안녕하세요");
+        System.out.println(12);  // 숫자
+        System.out.println(-34);
+        System.out.println(3.14);
+        System.out.println(true);  // 불리언
+        System.out.println(false);
+
+        System.out.println(123 + 345);
+    }
+}
