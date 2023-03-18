f8fe41ccc9968746ac68c0bc16bf1ad835287195
woominhee <maya2001bee@naver.com>
2023-03-18 21:53:53 +0900

    01_Quiz_01

---
+++ JavaWorkspace/src/chap_01/_Quiz_01.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String busNo = "상암08";  // 버스 번호
+        int minute = 3;  // 남은 시간
+        double distance = 1.2;  // 남은 거리
+
+        System.out.println(busNo + "번 버스");
+        System.out.println("약 " + minute + "분 후 도착");
+        System.out.println("남은 거리 " + distance + "km");
+    }
+}
