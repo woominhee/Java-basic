07e871769cb0ce52669a93c8e1dc0ae7c84415be
woominhee <maya2001bee@naver.com>
2023-03-21 23:51:37 +0900

    07_04_Method

--- JavaWorkspace/src/chap_07/BlackBox.java
+++ JavaWorkspace/src/chap_07/BlackBox.java
@@ -7,4 +7,28 @@ public class BlackBox {
     String color; // 색상
 
     static boolean canAutoReport = false; // 자동 신고 기능, static 이 붙으면 클래스 변수, 모든 객체에 동일하게 적용됨
+
+    void autoReport() {
+        if (canAutoReport) {
+            System.out.println("충돌이 감지되어 자동으로 신고합니다.");
+        }
+        else {
+            System.out.println("자동 신고 기능이 지원되지 않습니다.");
+        }
+    }
+
+    void insertMemoryCard(int capacity) {
+        System.out.println("메모리 카드가 삽입되었습니다.");
+        System.out.println("용량은 " + capacity + "GB 입니다.");
+    }
+
+    int getVideoFileCount(int type) {
+        if (type == 1) { // 일반 영상
+            return 9;
+        }
+        else if (type == 2) { // 이벤트 영상
+            return 1;
+        }
+        return 10;
+    }
 }
