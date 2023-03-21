c4b545cacea56237cc26de1d99e0bdefcf5dea9d
woominhee <maya2001bee@naver.com>
2023-03-21 23:42:49 +0900

    07_03_ClassVariables

--- JavaWorkspace/src/chap_07/BlackBox.java
+++ JavaWorkspace/src/chap_07/BlackBox.java
@@ -1,8 +1,10 @@
 package chap_07;
 
 public class BlackBox {
-    String modelName; // 모델명
+    String modelName; // 모델명, 인스턴스 변수
     String resolution; // 해상도
     int price; // 가격
     String color; // 색상
+
+    static boolean canAutoReport = false; // 자동 신고 기능, static 이 붙으면 클래스 변수, 모든 객체에 동일하게 적용됨
 }
